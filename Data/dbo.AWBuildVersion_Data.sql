6f926b0b1bad7e057a781eb48a12885a9f1cef52
sandman153 <boppudsara@gmail.com>
2018-06-27 10:22:44 +1200

    Initial Commit of my adventureworks

---
+++ Data/dbo.AWBuildVersion_Data.sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[AWBuildVersion] ON
+INSERT INTO [dbo].[AWBuildVersion] ([SystemInformationID], [Database Version], [VersionDate], [ModifiedDate]) VALUES (1, N'13.0.1601.5', '2016-04-30 00:47:02.260', '2018-06-27 10:15:46.867')
+SET IDENTITY_INSERT [dbo].[AWBuildVersion] OFF
